60058971131cb4569670b6ab48916d7e1553c318
Wellington Silva <wsilva@usp.br>
2014-11-24 02:16:25 -0200

    Spliting database file into a train file and into a test file and improving cli messages

--- python/humor-classifier.py
+++ python/humor-classifier.py
@@ -2,8 +2,50 @@
 # -*- coding: utf-8 -*-
 import re
 import sys
+import random
 
-def readFileToLists(csvfile='../Sentiment Analysis Dataset.csv'):
+def separateTrainAndTestGroups(csvfile='Sentiment Analysis Dataset.csv', testSetPercentSize=0.3):
+    
+    try:
+        # open database file and sort it 'randomly'
+        with open(csvfile,'r') as source:
+            data = [ (random.random(), line) for line in source ]
+        data.sort()
+
+        # get the limit of lines on train file
+        trainSetPercentSize = 1 - testSetPercentSize
+        trainSetLimitLines = trainSetPercentSize * len(data)
+
+        # writting 2 files with trainSet and testSet based on limit lines on each
+        cont = 0
+        with open('trainSetFile.csv','w') as trainFile, open('testSetFile.csv','w') as testFile:
+            for _, line in data:
+                if cont < trainSetLimitLines:
+                    trainFile.write(line)
+                else:
+                    testFile.write(line)
+                cont+=1
+
+        # closing the resources
+        source.close()
+        trainFile.close()
+        testFile.close()
+
+        return True
+
+    except IOError as e:
+        source.close()
+        trainFile.close()
+        testFile.close()
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
+    except:
+        source.close()
+        trainFile.close()
+        testFile.close()
+        print "Unexpected error:", sys.exc_info()[0]
+        raise
+
+def readFileToLists(csvfile='trainSetFile.csv'):
 
     try:
         arq = open(csvfile, 'r')
@@ -52,11 +94,34 @@ def setupVocabulary(twittes):
 
     return vocabulary
 
+def getProbabilities(vocabulary):
+
+    for words,frequency in vocabulary:
+        prob_positive = float(freq["1"]) / float(self.count_positive)
+        prob_negative = float(freq["0"]) / float(self.count_negative)
+
 def main():
-    print "Lendo arquivo para a memória..."
-    twittes = readFileToLists()
+
+    print "Embaralhando dados e separando em conjuntos de treinamento e teste..."
+    separateTrainAndTestGroups("../Sentiment Analysis Dataset.csv", 0.3)
+    print "Concluído"
+    print "..."
+
+    print "Lendo arquivo de treinamento para a memória"
+    twittes = readFileToLists("trainSetFile.csv")
+    print "Concluído"
+    print "..."
+
+    print "Montando Vocabulário"
     vocabulary = setupVocabulary(twittes)
-    print "Vocabulário montado"
+    print "Concluído"
+    print "..."
+
+    print "Calculando prioris"
+    # prob = getProbabilities(vocabulary)
+    print "Concluído"
+    print "..."
+
     print "Fim"
 
 if __name__ == '__main__':
