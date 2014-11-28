99b3165f00856bdef955963e6c2918a5f0ab6e2c
Wellington Silva <wsilva@usp.br>
2014-11-28 19:37:57 -0200

    10 fold cross validation working

--- python/humor-classifier.py
+++ python/humor-classifier.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import os, time, sys
+import os, time, sys, itertools
 import re
 import random
 import NaiveBayesWordClassifier
@@ -111,9 +111,8 @@ def readFileToLists(csvfile='trainSetFile.csv'):
     arq.close()
 
     return documents
-    
-def main():
 
+def holdoutFlow():
     print "Embaralhando dados e separando em conjuntos de treinamento (30% trainSetFile.csv) e teste (70% testSetFile.csv)..."
     separateTrainAndTestGroupsUsingHoldout("../Sentiment Analysis Dataset.csv", 0.3)
     print "Concluído"
@@ -137,7 +136,7 @@ def main():
     print "ni (número total da frequência de palavras de cada classe):"
     print "     n[pos] = %s" % (nb.freq['1'])
     print "     n[neg] = %s" % (nb.freq['0'])
-    print "P(wi|ci) - Probabilidade condicional de cada palaavra dada uma classe."
+    print "P(wi|ci) - Probabilidade condicional de cada palavra dada uma classe."
     print "Limitando quantidade de exemplos em 5"
     cont=0
     for word, prob in nb.probability.iteritems():
@@ -173,8 +172,70 @@ def main():
     print "%.2f %% de acertos" % (100 * float(hits)/float(len(listOfDocuments)))
     print ""
 
-    print "Embaralhando dados e separando em 10 conjuntos (crossvalidation-{1..10}.csv)"
-    separateTrainAndTestGroupsUsingCrossvalidation("../Sentiment Analysis Dataset.csv")
+def crossvalidationFlow():
+    qtdeFolds = 10
+    print "Embaralhando dados e separando em %s conjuntos (crossvalidation-{1..%s}.csv)" % (qtdeFolds, qtdeFolds)
+    separateTrainAndTestGroupsUsingCrossvalidation("../Sentiment Analysis Dataset.csv", qtdeFolds)
+    print "Concluído"
+    print ""
+
+    print "Preparando as rodadas de crossvalidation"
+    trainList={}
+    testList={}
+    for j in range(1, 1+qtdeFolds):
+        print "Rodada %s do crossvalidation" % j
+        for i in range(1, 1+qtdeFolds):
+            filename = 'crossvalidation-{}.csv'.format(i)
+            tmpList = readFileToLists(filename)
+            if i==j:
+                testList[j] = tmpList
+                print "Arquivo %s separado para teste" % filename
+            else:
+                if j in trainList:
+                    trainList[j] = itertools.chain(trainList[j], tmpList)
+                else:
+                    trainList[j] = tmpList
+                print "Arquivo %s separado para treino" % filename
+    print "Concluído"
+    print ""
+
+    print "Usando o naive bayes nas rodadas do crossvalidation"
+    hitsList=[]
+    missList=[]
+    for i in range(1,1+qtdeFolds):
+        print "Treinamento número %s" % i
+        nb = NaiveBayesWordClassifier.NaiveBayesWordClassifier()
+        nb.train(trainList[i])
+
+        hits = 0 #contador de acertos
+        miss = 0 #contador de acertos
+        print "Teste número %s" % i
+        for doc in testList[i]:
+
+            # attributes from the list
+            document = doc[0]
+            sentiment = doc[1]
+            tweet = doc[2]
+            identifiedClass = nb.classificator(tweet)
+            
+            # counting the right hits
+            if sentiment==identifiedClass:
+                hits+=1
+            else:
+                miss+=1
+        
+        missList.append(100 * float(miss)/float(len(testList[i])))
+        hitsList.append(100 * float(hits)/float(len(testList[i])))
+
+    print "Média de acertos: %.2f %%" % (sum(hitsList) / float(len(hitsList)))
+    print "Média de erros: %.2f %%" % (sum(missList) / float(len(missList)))
+    
+def main():
+
+    holdoutFlow()    
+
+    # crossvalidationFlow()
+
     print "Concluído"
     print ""
 
